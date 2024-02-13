beb757b14168d0e7d30eadac592f41166f492302
Rhys Clarey <rebo@sky.com>
2024-02-13 21:13:40 +0000

    13022024 Email Validation

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Middleware\{TemplateDataMiddleware, ValidatioNExceptionMiddleware, SessionMiddleware};
+use App\Middleware\{TemplateDataMiddleware, ValidatioNExceptionMiddleware, SessionMiddleware, FlashMiddleware};
 
 function registerMiddleware(App $app)
 {
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidatioNExceptionMiddleware::class);
+    $app->addMiddleware(FlashMiddleware::class);
+    //This needs to be last so other middleware has access to this class.
     $app->addMiddleware(SessionMiddleware::class);
 }
