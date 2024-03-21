3df3d8082100365d859a77cb48c4043d649dba00
Rhys Clarey <rebo@sky.com>
2024-03-21 17:06:42 +0000

    21032024 CSRF

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Middleware\{TemplateDataMiddleware, ValidatioNExceptionMiddleware, SessionMiddleware, FlashMiddleware, CsrfTokenMiddleware};
+use App\Middleware\{TemplateDataMiddleware, ValidatioNExceptionMiddleware, SessionMiddleware, FlashMiddleware, CsrfTokenMiddleware, CsrfGuardMiddleware};
 
 function registerMiddleware(App $app)
 {
+    $app->addMiddleware(CsrfGuardMiddleware::class);
     $app->addMiddleware(CsrfTokenMiddleware::class);
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidatioNExceptionMiddleware::class);
